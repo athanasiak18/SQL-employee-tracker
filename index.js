c591678da17e56eeacd251763ad4862b808a04f4
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 08:13:54 -0400

    util

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const table = require('console.table');
+const inquirer = require("inquirer");
+const util = require('util');
+const consoleTable = require("console.table");
