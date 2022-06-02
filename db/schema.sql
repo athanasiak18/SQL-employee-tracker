177e48e5c022c6caa08912ab18e8db775724c823
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 08:25:33 -0400

    table

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXISTS employeedatabase2022;
 CREATE DATABASE employeedatabase2022;
 
 USE employeedatabase2022;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
