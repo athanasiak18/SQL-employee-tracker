88b2c5a2932869de6114db3203f52974c1e1b7fd
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 08:20:49 -0400

    check

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -1,7 +1,10 @@
 const inputCheck = require('../utils/inputCheck');
 
-test('inputCheck() returns null when all properties exist', () => {
+test('inputCheck()', () => {
     const obj = {name: 'martha'};
 
     expect(inputCheck(obj, 'name')).toBe(null);
 });
+
+test('inputCheck()', () => {
+    const obj = {name: 'martha', occupation: ''};
