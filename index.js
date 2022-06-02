74bb341bb9b2d1c6b9df4951ff06edfd905d0976
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 08:14:53 -0400

    db connect

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const util = require('util');
 const consoleTable = require("console.table");
+
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
